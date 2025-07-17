bc832af5622f9f4440419c5327368ec64e01a36c
aureviee <hanyulixx@gmail.com>
2025-07-17 16:50:02 +0800

    vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: './', // ✅ 使用相對路徑才能部署在 root
+  base: './',
   plugins: [react()],
 })
